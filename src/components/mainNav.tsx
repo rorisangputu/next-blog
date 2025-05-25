c6dbb840eaeb93073ca970b4d804c66caf098252
Rorisang Putu <rorisangputu@gmail.com>
2025-05-25 20:00:40 +0200

    refactor: improve MainNav structure and styling for better layout and readability

--- src/components/mainNav.tsx
+++ src/components/mainNav.tsx
@@ -1,18 +1,26 @@
+import Link from "next/link";
 import React from "react";
+import { Icons } from "./icons";
 
 const MainNav = () => (
-  <div className="py-10 flex flex-row justify-between">
-    <div>
+  <div className="py-10 flex flex-col gap-4 items-center md:gap-0  md:flex-row md:justify-between z-50">
+    <Link href={"/"} className="flex items-center gap-2">
+      <Icons.logo className="h-6 w-6" />
       <h1 className="font-bold text-2xl">Rori Blog</h1>
-    </div>
-    <div>
+    </Link>
+    <div className="flex flex-row gap-10">
       <ul className="flex flex-row gap-10">
         {links.map((link) => (
-          <li className="font-medium hover:text-blue-400" key={link.link}>
-            {link.title}
-          </li>
+          <Link href={link.link} key={link.link}>
+            <li className="font-medium hover:text-blue-400">{link.title}</li>
+          </Link>
         ))}
       </ul>
+      <div>
+        <Link href={"/rss"}>
+          <Icons.rss className="h-6 w-6" />
+        </Link>
+      </div>
     </div>
   </div>
 );
@@ -23,6 +31,10 @@ const links = [
   {
     title: "Home",
     link: "/",
+  },
+  {
+    title: "Posts",
+    link: "/posts",
   },
   {
     title: "About",
