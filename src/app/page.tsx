0ca5865eeb57b2fe52e7791a58397031555d1391
Rorisang Putu <rorisangputu@gmail.com>
2025-05-25 19:26:04 +0200

    refactor: remove MainNav component from Home page for simplification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import LatestPosts from "@/components/home/latest-post";
-import MainNav from "@/components/mainNav";
 
 export default function Home() {
   return (
     <>
-      <MainNav />
       <main>
         <div>
           <LatestPosts />
