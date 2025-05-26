948ca3404ca0ec6ec1952c9299e5caafeb281abc
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:33:25 +0200

    fix: wrap Home component in Container for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import Container from "@/components/Container";
 import LatestPosts from "@/components/home/latest-post";
+import MainNav from "@/components/mainNav";
 
 export default function Home() {
   return (
-    <>
+    <Container>
+      <MainNav />
+      <hr className="w-[30%] mx-auto mb-10 text-gray-300" />
       <main>
         <div>
           <LatestPosts />
         </div>
       </main>
-    </>
+    </Container>
   );
 }
