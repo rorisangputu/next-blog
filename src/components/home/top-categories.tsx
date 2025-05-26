bc48c34bdb356bb5fe4f8a412b8de53d5b08db05
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:55:12 +0200

    fix: enhance button styling in TopCategories component for improved interactivity

--- src/components/home/top-categories.tsx
+++ src/components/home/top-categories.tsx
@@ -6,7 +6,8 @@ export default function TopCategories() {
     <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
       {categories.map((category) => (
         <button
-          className="bg-gray-200 text-gray-800 shadow-gray-300 shadow-sm text-sm font-semibold p-3 rounded-lg"
+          className="bg-gray-200 text-gray-800 shadow-gray-300 shadow-sm 
+            text-sm font-semibold p-3 rounded-lg hover:scale-110 transition-all"
           key={category}
         >
           <Link href={`/blog/${category}`}>
