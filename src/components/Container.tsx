2e02c804be7ed211d68276dc0843bca8867d4f2d
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:27:53 +0200

    fix: add responsive padding to Container component for improved layout

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 const Container = ({
-  className,
   children,
 }: {
   className?: string;
   children: React.ReactNode;
 }) => {
   return (
-    <div className="mx-auto w-full max-w-screen-xl px-2.5">
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
       {children}
     </div>
   );
