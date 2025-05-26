1c6a7e83196a0df20bf0d0b83c0c3186160adfc7
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 21:30:13 +0200

    feat: wrap list items in PopularPosts component with Link for improved navigation

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,13 +1,16 @@
 import { popularposts } from "@/lib/place-data";
 import { Icons } from "../icons";
+import Link from "next/link";
 export default function PopularPosts() {
   return (
     <ul className="overflow-auto">
       {popularposts.map((post, i) => (
-        <li className="flex items-center gap-2 cursor-pointer py-2" key={i}>
-          <Icons.arrowRight className="h-6 w-6" />
-          <p>{post.title}</p>
-        </li>
+        <Link href={`/blog/`} key={i}>
+          <li className="flex items-center gap-2 cursor-pointer py-2">
+            <Icons.arrowRight className="h-6 w-6 hover:translate-x-1 transition-all" />
+            <p>{post.title}</p>
+          </li>
+        </Link>
       ))}
     </ul>
   );
