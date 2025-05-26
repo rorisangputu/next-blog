83ca1b88d39e3120178b1119cbf9aecdceadc0b5
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:55:09 +0200

    feat: add popular posts data for enhanced content display

--- src/lib/place-data.ts
+++ src/lib/place-data.ts
@@ -6,3 +6,18 @@ export const categories = [
   "performance",
   "database",
 ];
+
+export const popularposts = [
+  {
+    title: "React Vibe Coding",
+  },
+  {
+    title: "PostgreSQL: All you need to know",
+  },
+  {
+    title: "Creating faster blogs with MDX",
+  },
+  {
+    title: "React 19: The good, the bad, the interesting",
+  },
+];
