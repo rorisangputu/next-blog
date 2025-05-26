c35541c1098395e6b37d818a3f856bd449d81f2f
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 22:53:28 +0200

    refactor: update TopCategories component to use POSTS constant for category data

--- src/components/home/top-categories.tsx
+++ src/components/home/top-categories.tsx
@@ -1,17 +1,17 @@
-import { categories } from "@/lib/place-data";
+import { POSTS } from "@/lib/constants";
 import Link from "next/link";
 
 export default function TopCategories() {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-3">
-      {categories.map((category) => (
+      {POSTS.map((category) => (
         <button
           className="bg-gray-200 text-gray-800 shadow-gray-300 shadow-sm 
             text-sm font-semibold p-3 rounded-lg hover:scale-110 transition-all"
-          key={category}
+          key={category.title}
         >
-          <Link href={`/blog/${category}`}>
-            <p>{category}</p>
+          <Link href={`/blog/${category.href}`}>
+            <p>{category.title}</p>
           </Link>
         </button>
       ))}
