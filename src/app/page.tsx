1c456aeeb61085c50c02fc0a81a55e5151cd08c3
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:39:53 +0200

    fix: update layout structure in Home component for improved organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,20 @@ export default function Home() {
   return (
     <Container>
       <MainNav />
-      <hr className="w-[30%] mx-auto mb-10 text-gray-300" />
-      <main>
+      <hr className="w-[40%] mx-auto mb-10 text-gray-300" />
+      <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
         <div>
           <LatestPosts />
+        </div>
+        <div>
+          <div>
+            <h1>Top categories</h1>
+            {/* <TopCategories /> */}
+          </div>
+          <div>
+            <h1>Top Posts</h1>
+            {/* <PopularPosts /> */}
+          </div>
         </div>
       </main>
     </Container>
