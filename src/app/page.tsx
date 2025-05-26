e4f5f5bb266bed146e3df0c6c15580d777353c1c
Rorisang Putu <rorisangputu@gmail.com>
2025-05-26 20:49:55 +0200

    feat: integrate TopCategories component into Home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import LatestPosts from "@/components/home/latest-post";
+import TopCategories from "@/components/home/top-categories";
 import MainNav from "@/components/mainNav";
 
 export default function Home() {
@@ -11,13 +12,13 @@ export default function Home() {
         <div>
           <LatestPosts />
         </div>
-        <div>
+        <div className="h-screen">
           <div>
-            <h1>Top categories</h1>
-            {/* <TopCategories /> */}
+            <h1 className="font-bold mb-4 capitalize">Top categories</h1>
+            <TopCategories />
           </div>
-          <div>
-            <h1>Top Posts</h1>
+          <div className="mt-10 sticky top-5">
+            <h1 className="font-bold mb-4">Top Posts</h1>
             {/* <PopularPosts /> */}
           </div>
         </div>
