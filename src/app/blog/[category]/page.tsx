6ad8f8d4a684c405e8fa7873cf8ddf337a943995
Rorisang Putu <rorisangputu@gmail.com>
2025-05-27 19:47:36 +0200

    feat: implement blog category page with post filtering and sorting

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,3 +1,35 @@
+import { notFound } from "next/navigation";
+import { getBlogPosts } from "../utils";
+import Link from "next/link";
+
 export default function Page({ params }: { params: { category: string } }) {
-  return <>{params.category}</>;
+  const category = params.category;
+  const posts = getBlogPosts().filter(
+    (post) => post.metadata.category === category
+  );
+
+  if (posts.length === 0) {
+    notFound();
+  }
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+        {posts
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) >
+              new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post, i) => (
+            <Link key={i} href={`/blog/${post.metadata.category}/${post.slug}`}>
+              {post.metadata.title}
+            </Link>
+          ))}
+      </div>
+    </>
+  );
 }
