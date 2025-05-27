c53382d22e795ae20c5e77c631715de62887e065
Rorisang Putu <rorisangputu@gmail.com>
2025-05-27 20:17:33 +0200

    fix: ensure main content area expands to fill available space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
