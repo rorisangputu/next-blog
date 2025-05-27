6c2eeb324be0e3ac81de346fb08d2847df6a2275
Rorisang Putu <rorisangputu@gmail.com>
2025-05-27 20:50:19 +0200

    feat: add Header component to blog category page for improved layout

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -3,6 +3,7 @@ import { getBlogPosts } from "../utils";
 import Link from "next/link";
 import Container from "@/components/Container";
 import CardCategory from "@/components/CardCategory";
+import Header from "@/components/Header";
 
 export default function Page({ params }: { params: { category: string } }) {
   const category = params.category;
@@ -10,11 +11,18 @@ export default function Page({ params }: { params: { category: string } }) {
     (post) => post.metadata.category === category
   );
 
-  if (posts.length === 0) {
+  if (!posts.length) {
     notFound();
   }
   return (
     <>
+      <Header>
+        <Container>
+          <h1 className="title font-semibold text-2xl tracking-wider uppercase">
+            {posts[0]?.metadata.category}
+          </h1>
+        </Container>
+      </Header>
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
           {posts
