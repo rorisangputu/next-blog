86eb49e44df8ca0f2cbaef7963c447b3361b89a6
Rorisang Putu <rorisangputu@gmail.com>
2025-05-27 20:17:27 +0200

    feat: wrap blog posts in Container and use CardCategory for improved display

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,6 +1,8 @@
 import { notFound } from "next/navigation";
 import { getBlogPosts } from "../utils";
 import Link from "next/link";
+import Container from "@/components/Container";
+import CardCategory from "@/components/CardCategory";
 
 export default function Page({ params }: { params: { category: string } }) {
   const category = params.category;
@@ -13,23 +15,32 @@ export default function Page({ params }: { params: { category: string } }) {
   }
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
-        {posts
-          .sort((a, b) => {
-            if (
-              new Date(a.metadata.publishedAt) >
-              new Date(b.metadata.publishedAt)
-            ) {
-              return -1;
-            }
-            return 1;
-          })
-          .map((post, i) => (
-            <Link key={i} href={`/blog/${post.metadata.category}/${post.slug}`}>
-              {post.metadata.title}
-            </Link>
-          ))}
-      </div>
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+          {posts
+            .sort((a, b) => {
+              if (
+                new Date(a.metadata.publishedAt) >
+                new Date(b.metadata.publishedAt)
+              ) {
+                return -1;
+              }
+              return 1;
+            })
+            .map((post, i) => (
+              <Link
+                key={i}
+                href={`/blog/${post.metadata.category}/${post.slug}`}
+              >
+                <CardCategory
+                  title={post.metadata.title}
+                  summary={post.metadata.summary}
+                  publishedAt={post.metadata.publishedAt}
+                />
+              </Link>
+            ))}
+        </div>
+      </Container>
     </>
   );
 }
