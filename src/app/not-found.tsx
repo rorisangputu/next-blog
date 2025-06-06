6237c3977e1113336b92ea4b7f95038e225aafc8
Rorisang19 <rorisangputu@gmail.com>
2025-06-06 09:18:42 +0200

    feat: add NotFound component for handling 404 errors with user-friendly message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <section>
-        <h1>404 - Page Not Found</h1>
-        <p>This page you are looking for does not exist.</p>
+    <section className='grid place-content-center h-screen'>
+        <h1 className='mb-3 text-2xl font-semibold tracking-tighter'>404 - Page Not Found</h1>
+        <p className='mb-4'>This page you are looking for does not exist.</p>
         <Link href={'/'}>
             Go Home.
         </Link>
