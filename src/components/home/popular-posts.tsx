0f1691065f6bde8c72a6f6e43712eaa448b77477
Rorisang Putu <rorisangputu@gmail.com>
2025-06-07 16:04:42 +0200

    refactor: replace static popular posts with dynamic fetching using SWR

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,11 +1,38 @@
-import { popularposts } from "@/lib/place-data";
+// import { popularposts } from "@/lib/place-data";
+import { fetcher, fetchUrl } from "@/lib/utils";
 import { Icons } from "../icons";
 import Link from "next/link";
+import useSWR from "swr";
+
+type PostData = {
+  category: string;
+  slug: string;
+  title: string;
+};
+
 export default function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) {
+    return (
+      <div>
+        <h1>Failed to load</h1>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
+
   return (
     <ul className="overflow-auto">
-      {popularposts.map((post, i) => (
-        <Link href={`/blog/`} key={i}>
+      {data?.map((post: PostData) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
           <li className="flex items-center gap-2 cursor-pointer py-2">
             <Icons.arrowRight className="h-6 w-6 hover:translate-x-1 transition-all" />
             <p>{post.title}</p>
