9b8d9bb031ae07e29a23b8076e5254bc5958909e
Rorisang Putu <rorisangputu@gmail.com>
2025-06-07 12:40:38 +0200

    refactor: simplify POST request handling and remove redundant database checks

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,39 +1,31 @@
 import { db } from "@/db";
 
-export async function GET(){
-    return new Response("Hello", {status: 200});
+export async function GET() {
+  return new Response("Hello", { status: 200 });
 }
 
-export async function POST(request:Request) {
-    const { slug, category, title } = await request.json();
-    
-    try {
-        const existingPost = await db.blog.findUnique({
-            where: {slug: slug},
+export async function POST(request: Request) {
+  const { slug, category, title } = await request.json();
 
-        });
+  try {
+    await db.blog.upsert({
+      where: { slug }, // Unique identifier
+      update: {
+        view_count: {
+          increment: 1,
+        },
+      },
+      create: {
+        slug,
+        title,
+        category,
+        updatedAt: new Date(),
+      },
+    });
+  } catch (error) {
+    console.error("Error updating view: ", error);
+    return new Response("Failed to post to DB", { status: 500 });
+  }
 
-        if(existingPost){
-            await db.blog.update({
-                where: {slug: slug},
-                data: {
-                    view_count: { increment: 1 }
-                }
-            })
-        }else {
-            await db.blog.create({
-                data: {
-                    slug: slug,
-                    title: title,
-                    category: category,
-                    updatedAt: new Date()
-                }
-            });
-        }
-    } catch (error) {
-        console.error("Error updating view: ", error );
-        return new Response("Failed to post to DB", { status: 500})
-    }
-
-    return new Response("Successfully posted to DB", {status: 200 })
+  return new Response("Successfully posted to DB", { status: 200 });
 }
