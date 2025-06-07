c42531a5adc943cfe630364a697fef5308b4f9a4
Rorisang Putu <rorisangputu@gmail.com>
2025-06-07 16:05:08 +0200

    refactor: remove unnecessary whitespace in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,12 +3,11 @@ export const fetchUrl =
     ? "http://localhost:3000/api"
     : "https://next-blog-swart-mu.vercel.app/api";
 
-    
 type ResponseData = {
   category: string;
   title: string;
   slug: string;
-};
+}[];
 export const fetcher = (
   ...args: Parameters<typeof fetch>
 ): Promise<ResponseData> => fetch(...args).then((res) => res.json());
