11eb378b8eaaaa4f7ddde8c29b0ab2e070651686
Rorisang Putu <rorisangputu@gmail.com>
2025-06-08 14:09:59 +0200

    feat: update metadata configuration to use site config values

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
+import { siteConfig } from "@/config/site";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,50 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Blog App",
-  description: "Rorisang Putu",
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Rorisang Putu",
+    "SQL",
+    "PostgreSql",
+    "Javascript",
+    "Typescript",
+    "AWS",
+    "C#",
+  ],
+  authors: [
+    {
+      name: "Rorisang Putu",
+      url: "https://github.com/rorisangputu",
+    },
+  ],
+  creator: "rorisang putu",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og`],
+    creator: "@df_rori",
+  },
+  icons: {
+    icon: "/logo.ico",
+    shortcut: "/logo.ico",
+    apple: "logo.ico",
+  },
 };
 
 export default function RootLayout({
